de8c521e77656484f05157fc050e93edecdcfa3f
aadarshg25 <111424028+aadarshg25@users.noreply.github.com>
2023-11-01 13:03:36 +0000

    Variables

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,13 @@
+const acccountId = 12345
+let email = "harrypotter@new.com"
+var acccountPassword = "12346"
+acccountCity = "Jaipur"
+let acccountState;
+// acccountId = 216655 //Not Allowed
+
+/*
+Prefer not to use var 
+because of block scope and functional scope
+*/
+console.log(acccountId);
+console.table([acccountId,email,acccountPassword,acccountCity,acccountState])
