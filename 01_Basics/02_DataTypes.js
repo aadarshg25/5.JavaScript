45d70a8d1ee0f34da4232950a68d78997b6a280c
aadarshg25 <111424028+aadarshg25@users.noreply.github.com>
2023-11-01 13:03:52 +0000

    Datatypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,20 @@
+"use strict";// treats all js code as newer version
+
+// alert("hello") // we are using node js not browser
+
+let number = 122224;
+let name = "aadarsh"
+let isLogged = true;
+
+// number => range 2 to power 53
+// bigint => for big integers
+//String  => ""
+//boolean => true/false
+//null = > standalone value (empty value)
+//undefined => abhi value assign nhi hui hain
+//symbol = > uniquen
+
+//object
+
+console.log(typeof null); // object
+console.log(typeof undefined);// undefined
