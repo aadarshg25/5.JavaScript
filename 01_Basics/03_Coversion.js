796656abc60747227a1444c256c3b908ffac92db
aadarshg25 <111424028+aadarshg25@users.noreply.github.com>
2023-11-01 13:48:43 +0000

    Conversion

---
+++ 01_Basics/03_Coversion.js
@@ -0,0 +1,26 @@
+let score = undefined
+
+//console.log(typeof(score));
+
+let valueOfScore = Number(score);
+//console.log(valueOfScore);
+//console.log(typeof valueOfScore);
+
+//NaN(Not A Number) => type : Number
+// "33" => 33
+//"33a" = > NaN
+// true => 1 ; false = > 0
+// null = > 0
+// undefined =>NaN
+
+let isLoggedIn = "Aadarsh" ;
+let valueOfIsLoggedIn = Boolean(isLoggedIn);
+console.log(valueOfIsLoggedIn);
+
+// 1 => true | 0 => false | number other than zero => true
+//"" => false | "abs" => true
+
+let someNumber = 23;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
