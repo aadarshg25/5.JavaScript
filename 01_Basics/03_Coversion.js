a010f1deab2f2594f570d32d66f5ae1d3b04b0a7
aadarshg25 <111424028+aadarshg25@users.noreply.github.com>
2024-01-08 08:20:06 +0000

    commit

--- 01_Basics/03_Coversion.js
+++ 01_Basics/03_Coversion.js
@@ -3,8 +3,8 @@ let score = undefined
 //console.log(typeof(score));
 
 let valueOfScore = Number(score);
-//console.log(valueOfScore);
-//console.log(typeof valueOfScore);
+//console.log(valueOfScore); //NaN
+//console.log(typeof valueOfScore); // Number
 
 //NaN(Not A Number) => type : Number
 // "33" => 33
@@ -24,3 +24,27 @@ let someNumber = 23;
 let stringNumber = String(someNumber);
 console.log(typeof stringNumber);
 console.log(stringNumber);
+console.log("---------------------OPERATIONS---------------------------");
+// ******************* OPERATIONS **************************************************
+
+let value = 3 ;
+let negValue = - value;
+// console.log(negValue); //-3
+
+//console.log(2**3); // 8 --> 2 to power 3 (**)
+
+let  str1 = "hello";
+let str2 = " Aadarsh";
+let str = str1 + str2;
+// any operator except  + will give NaN
+// console.log(str); // hello Aadarsh
+
+// console.log(1 + "2"); //12
+// console.log("1" + 2); //12
+// console.log("1" + 2 + 2 ); //122
+// console.log(1 + 2 + "2") //32
+
+// console.log(+true) //1 but true+ gives error
+// console.log(+false)
+
+// console.log(+""); //0
