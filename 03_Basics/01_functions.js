9bd6725576d3109527629e13eb66dc71deade112
Aadarsh Gupta <111424028+aadarshg25@users.noreply.github.com>
2024-01-17 11:58:37 +0000

    ss

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -48,4 +48,38 @@ function loginUserMessage(userName = "Harry"){
     return `${userName} just logged in`
 }
 //console.log(loginUserMessage("hello")); //hello just logged in // loginUserMessage("hello") if you just write this nothing will be printed
-console.log(loginUserMessage("Nobody")); // undefined just logged in
+// console.log(loginUserMessage("Nobody")); // undefined just logged in // Nobody just logged in
+
+//----------------------------------------------------------------------------------------------------------
+
+//Multiple parameters 
+
+//(...) is knows as both "Rest"  and "Spread" it depends on the usecase you are using
+function calculateCartPrice(val1,val2,...num1){ //  <------ this is rest operator it pack the arguments passed into an array
+    return num1;
+}
+//console.log(calculateCartPrice(100,200,300,5000,20,1,620)) //[ 300, 5000, 20, 1, 620 ] <-num1 and val1->100 and val2 ->200
+
+
+//Objects as parameters
+
+const user = {
+    userName : "Aadarsh",
+    age : 21
+}
+
+function handleObject(anyUser){
+    console.log(`${anyUser.userName} is ${anyUser.age} years old`);
+}
+//handleObject(user) // if you pass anything other than object our it dosent have the key then it doesnt give error it gives undefined
+//Aadarsh is 21 years old 
+
+//Arrays as parameters
+
+const newArray = [100,200,300,400]
+
+function returnSecondValue(anyArray){
+    return anyArray[1];
+}
+
+console.log(returnSecondValue(newArray)); //200
