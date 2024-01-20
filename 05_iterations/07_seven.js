52091b3f6fcc59eeeff75a9e6ee2237334f705c2
Aadarsh Gupta <111424028+aadarshg25@users.noreply.github.com>
2024-01-20 15:06:13 +0000

    ss

--- 05_iterations/07_seven.js
+++ 05_iterations/07_seven.js
@@ -1,9 +1,30 @@
 // Reduce method
 const myNUms = [1,2,3]
 
-const myTotal = myNUms.reduce(function (acc,currval){
-    console.log(`acc : ${acc} and currval : ${currval}`);
-    return acc + currval
-},0) // after callback fun. we pass the value of acc
+// const myTotal = myNUms.reduce(function (acc,currval){ // In this the currval denotes the item we are in like in foreach and all
+//     console.log(`acc : ${acc} and currval : ${currval}`);
+//     return acc + currval
+// },0) // after callback fun. we pass the value of acc
 
-console.log(myTotal);
+// Using arrow function
+const myTotal = myNUms.reduce((acc,currval)=>acc+currval,0)
+
+//console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName : "js course",
+        price : 2999
+    },
+    {
+        itemName : "java course",
+        price : 3999
+    },
+    {
+        itemName : "Python course",
+        price : 5999
+    },
+]
+
+const priceTOPay = shoppingCart.reduce((acc,item)=> acc + item.price,0)
+console.log(priceTOPay);
